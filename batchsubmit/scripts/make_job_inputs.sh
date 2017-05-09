3bdc6a83122ab9f800f1dbc314bd277d3fbfb8f2
Philip Chang <sgnoohc@gmail.com>
2017-05-09 11:03:24 -0700

    msg

---
+++ batchsubmit/scripts/make_job_inputs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "Creating a directory where the binaries and jobs to run are aggregated."
+echo "Creating $DIR/../__package"
+mkdir -p $DIR/../__package
+cd $DIR/../__package
+rm -f *
+cp $COREPATH/libCMS3_CORE.so .
+cp $DITTOPATH/bin/ditto_ana_mybaby .
+cp $DIR/../scripts/run_baby_run.sh .
+ln -s $COREPATH CORE
+tar -czf package.tar.gz CORE/Tools/jetcorr/data/run2_25ns/ libCMS3_CORE.so ditto_ana_mybaby run_baby_run.sh
+rm CORE
+rm libCMS3_CORE.so ditto_ana_mybaby
+cp $DIR/../scripts/sweeproot.sh $DIR/../scripts/sweeproot_macro.C $DIR/../scripts/merge_script.sh $DIR/../scripts/merge_macro.C .
+
+echo "Creatied $DIR/../__package"
+ls -l $DIR/../__package
